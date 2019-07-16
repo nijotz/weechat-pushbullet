8a769576de2f57609d27d32f8088c08dbde1d34f
Nick Tzaperas <nick@nijotz.com>
2019-07-16 11:30:34 -0700

    second commit

--- pushbullet.py
+++ pushbullet.py
@@ -1,16 +1,33 @@
 import weechat, urllib, json
 
-weechat.register("weechat-pushbullet", "soh335", "0.0.1", "MIT", "notify via pushbullet", "", "")
-weechat.hook_print("", "notify_private,notify_highlight", "", 1, "print_cb", "")
-weechat.hook_command("pushbullet", "send pushbullet for debug message", "[message]", "", "", "pushbullet_cb", "")
+weechat.register("pushbullet", "soh335", "0.0.1", "MIT", "notify via pushbullet", "", "")
+weechat.hook_print("", "notify_private,notify_highlight,notify_message", "", 1, "print_cb", "")
+weechat.hook_command("pushbullet", "send pushbullet a debug message", "[message]", "", "", "pushbullet_cb", "")
+weechat.config_set_plugin("notify_on_away", "true")
 
 def pushbullet_cb(data, buffer, args):
   notify(args)
   return weechat.WEECHAT_RC_OK
 
 def print_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
+  # Verify away
+  away_msg = weechat.buffer_get_string(buffer, "localvar_away")
+  away = True if away_msg and len(away_msg) > 0 else False
+  if not (weechat.config_get_plugin("notify_on_away") and away):
+    return weechat.WEECHAT_RC_OK
+
+  # Verify highlight or private message
+  buffer_type = weechat.buffer_get_string(buffer, "localvar_type")
+  if not (buffer_type == "private" or highlight):
+    return weechat.WEECHAT_RC_OK
+
+  # Filter certain notifications
+  if weechat.buffer_match_list(buffer, '*.chanserv,*.nickserv'):
+    return weechat.WEECHAT_RC_OK
+
+  # Send message
   buffer_name = weechat.buffer_get_string(buffer, "full_name")
-  body        = "[%s] <%s> %s" % (buffer_name, prefix, message)
+  body = "[%s] <%s> %s" % (buffer_name, prefix, message)
   notify(body)
   return weechat.WEECHAT_RC_OK
 
